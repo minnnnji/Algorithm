ab10d2161b10088b669558a92130a24fe3994e56
엄민지 <48341382+minnnnji@users.noreply.github.com>
2019-10-16 16:24:51 +0900

    Update In_Place_Insertion_Sort.c (#2)

--- SORT/In_Place_Insertion_Sort.c
+++ SORT/In_Place_Insertion_Sort.c
@@ -1,40 +1,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>;
-void swap(int *a, int*b) {
+#include<time.h>
+/*void swap(int *a,int *b) {
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
-}// 자리 바꾸기 함수
-void inPlaceInsertionSort(int *p, int n) {
-	int tmp, j;
-	for (int i = 1; i < n; i++) {
+}*/
+void InPlaceInsertionSort(int *p, int n) {
+	int tmp,i,j;
+	for (i = 0; i < n; i++) {
 		tmp = p[i];
 		j = i - 1;
-		while (j >= 0 && p[j] > tmp) {
+		while (j >= 0&&tmp<p[j]) {
 			p[j + 1] = p[j];
 			j--;
 		}
 		p[j + 1] = tmp;
 	}
 }
-void print_Array(int *p, int n) {
+void travel(int *p, int n) {
 	for (int i = 0; i < n; i++) {
 		printf(" %d", p[i]);
 	}
-	printf("\n");
-}// 배열 프린트하기
+	printf("\n\n");
+}
 int main() {
+	srand(time(NULL));
 	int n, *p = NULL;
 	scanf("%d", &n);
-	srand(time(NULL));
 	p = (int *)malloc(sizeof(int)*n);
 	for (int i = 0; i < n; i++) {
-		p[i] = rand() % 100 + 1;
+		p[i] = rand() % 10; //0 ~ 10까지의 수
+
 	}
-	print_Array(p, n);
-	inPlaceInsertionSort(p, n);
-	print_Array(p, n);
-	free(p);
+	printf("----------정렬전----------\n\n");
+	travel(p, n);
+	InPlaceInsertionSort(p, n);
+	printf("----------정렬후----------\n\n");
+	travel(p, n);
 }
