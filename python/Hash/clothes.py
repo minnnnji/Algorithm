047cc064ba8178ee52426982700fed0d4764ccaa
엄민지 <99x04@naver.com>
2021-12-03 13:04:48 +0900

    프로그래머스 level 2 : 위장

---
+++ python/Hash/clothes.py
@@ -0,0 +1,22 @@
+# clothes = [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
+clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+
+def solution(clothes):
+    answer = 0
+    cloths = {}
+    
+    for name, kind in clothes:
+        print(name, kind)
+        if kind in cloths:
+            cloths[kind]+=1
+        else:
+            cloths[kind] = 1
+    
+    print(cloths)
+    answer = 1
+    for key, value in cloths.items():
+        answer *= (value+1)
+        
+    return answer-1
+# 
+print(solution(clothes))
